dbdddcf199f2473958a21091caa19c7b86a09ea5
Hellon Canella <helloncanella@gmail.com>
2020-11-05 13:20:25 -0300

    ok

--- dist/style.7bbe.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see style.7bbe.js.LICENSE.txt */
-(self.webpackChunkwebpack_demo=self.webpackChunkwebpack_demo||[]).push([[296],{212:function(){}},0,[[212,666]]]);
